71356e0e24c8ac7e045900e56af2db2bbf3ca1f6
Saurabh Patel <saurabh.finch@gmail.com>
2020-12-24 11:34:49 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'Home' }" exact>
+              <img class="logo" src="./assets/build-a-bot-logo.png">Build-a-bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'Build' }" exact>
+              Build
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+<!--      <RobotBuilder />-->
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HomePage from './home/HomePage.vue';
+
+// import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  // components: {
+  //   RobotBuilder,
+  // },
 };
 </script>
 
 <style>
+  body {
+    background: linear-gradient(to bottom, #555, #999) fixed;
+  }
+</style>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
 }
 </style>
---
+++ src/parts/PartInfo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ part.title }}</h1>
+    <div>{{ part.description }}</div>
+  </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'PartInfo',
+  props: {
+    partType: { type: String },
+    id: {
+      type: [String, Number],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
+  computed: {
+    part() {
+      const { partType, id } = this;
+      return parts[partType].find((part) => part.id === +id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
